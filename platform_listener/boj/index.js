64c2258099090ed1bb37d1978d0423579e2f9ac9
dannysir <dannysir@naver.com>
2025-08-26 14:40:48 +0900

    fix : Arrow Func 제거 & 프로그래머스 가져올 위치 설정 Arrow Func 제거. 프로그래머스에서 리스트에서 정답을 가져오려고 했으나 비동기 처리로 인해 정확성이 떨어짐

--- platform_listener/boj/index.js
+++ platform_listener/boj/index.js
@@ -6,9 +6,9 @@ const option = {
     attributes: true,
 };
 
-const table_first_line = () => {
+function table_first_line() {
     const table_lines = document.querySelectorAll('tbody tr');
     return table_lines[0].querySelector(".result-text");
-};
+}
 
 observer.observe(table_first_line(), option);
--- platform_listener/programers/index.js
+++ platform_listener/programers/index.js
@@ -1,5 +1,3 @@
-import {process} from "./process";
-
 const button = document.querySelector("#submit-code");
 button.addEventListener('click', function () {
     const modal = document.querySelector(".modal");
@@ -17,3 +15,20 @@ button.addEventListener('click', function () {
 
     observer.observe(modal, option);
 })
+// 결과 비동기 처리로 인해 정확성이 떨어짐
+//
+// const observer = new MutationObserver(mutations => {
+//     process(mutations);
+// });
+//
+// const option = {
+//     attributes: true,
+// };
+//
+// function table_first_line  () {
+//     const table_lines = document.querySelectorAll('.SubmissionListstyle__ListItemColumnWrapper-sc-dysuo0-5');
+//     console.log(table_lines);
+//     return table_lines[1].querySelector(".SubmissionListstyle__ScoreText-sc-dysuo0-11");
+// }
+//
+// observer.observe(table_first_line(), option);
